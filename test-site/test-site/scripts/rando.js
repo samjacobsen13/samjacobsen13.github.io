ce759738fa06fed0594bb94153d3b0377916c6f5
samjacobsen13 <143229760+samjacobsen13@users.noreply.github.com>
2023-08-28 14:26:20 -0600

    Add files via upload

---
+++ test-site/test-site/scripts/rando.js
@@ -0,0 +1,13 @@
+// This is a comment.
+
+/* 
+So is this.
+This one can be mulitiple lines long.
+*/
+
+function setRandomNum() {
+    const rand = Math.random();
+    const elem = document.getElementById("rand_num");
+    elem.innerHTML = rand;
+}
+
